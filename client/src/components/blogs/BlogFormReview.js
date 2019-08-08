34255f6563b6e758caddb813634c1eba5f1e4f36
raghubandaru <raghubandaru45@gmail.com>
2019-08-08 16:21:25 +0530

    Image upload feature added to the blog post creation

--- client/src/components/blogs/BlogFormReview.js
+++ client/src/components/blogs/BlogFormReview.js
@@ -1,12 +1,15 @@
 // BlogFormReview shows users their form inputs for review
-import _ from 'lodash';
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import formFields from './formFields';
-import { withRouter } from 'react-router-dom';
-import * as actions from '../../actions';
+import _ from "lodash";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import formFields from "./formFields";
+import { withRouter } from "react-router-dom";
+import * as actions from "../../actions";
 
 class BlogFormReview extends Component {
+  state = {
+    file: null
+  };
   renderFields() {
     const { formValues } = this.props;
 
@@ -44,7 +47,11 @@ class BlogFormReview extends Component {
 
     const { submitBlog, history, formValues } = this.props;
 
-    submitBlog(formValues, history);
+    submitBlog(formValues, this.state.file, history);
+  }
+
+  onFileChange(event) {
+    this.setState({ file: event.target.files[0] });
   }
 
   render() {
@@ -52,6 +59,17 @@ class BlogFormReview extends Component {
       <form onSubmit={this.onSubmit.bind(this)}>
         <h5>Please confirm your entries</h5>
         {this.renderFields()}
+        <div style={{ marginBottom: "10px" }}>
+          <label htmlFor="img">Add an image</label>
+          <br />
+          <input
+            type="file"
+            name="blogImage"
+            id="img"
+            accept="image/*"
+            onChange={this.onFileChange.bind(this)}
+          />
+        </div>
 
         {this.renderButtons()}
       </form>
@@ -63,4 +81,7 @@ function mapStateToProps(state) {
   return { formValues: state.form.blogForm.values };
 }
 
-export default connect(mapStateToProps, actions)(withRouter(BlogFormReview));
+export default connect(
+  mapStateToProps,
+  actions
+)(withRouter(BlogFormReview));
---
+++ routes/uploadRoutes.js
@@ -0,0 +1,29 @@
+const AWS = require("aws-sdk");
+const uuid = require("uuid/v1");
+const keys = require("../config/keys");
+const requireLogin = require("../middlewares/requireLogin");
+
+const s3 = new AWS.S3({
+  accessKeyId: keys.accessKeyId,
+  secretAccessKey: keys.secretAccessKey,
+  signatureVersion: "v4",
+  region: "ap-south-1"
+});
+
+module.exports = app => {
+  app.get("/api/upload", requireLogin, (req, res) => {
+    const key = `${req.user.id}/${uuid()}.jpeg`;
+
+    s3.getSignedUrl(
+      "putObject",
+      {
+        Bucket: "adv-node",
+        ContentType: "image/jpeg",
+        Key: key
+      },
+      (err, url) => res.send({ key, url })
+    );
+  });
+};
+
+//ap-south-1
